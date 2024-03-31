0e713902e24cb4b63fb21721e922c674a819e292
dianaveselkina <vess_di@mail.ru>
2024-03-31 22:52:57 +0300

    add header

--- src/components/HeaderDiv.vue
+++ src/components/HeaderDiv.vue
@@ -1,11 +1,17 @@
 <template>
-  <div>
+  <div class="header">
     <h1>Hello!</h1>
   </div>
 </template>
 
 <style scoped>
+.header {
+  max-width: 1440px;
+  margin: 0 auto;
+}
 h1 {
-  color: #f00f0fe1;
+  color: #503f3fe1;
+  background-color: rgb(204, 124, 25);
+  text-align: center;
 }
 </style>
